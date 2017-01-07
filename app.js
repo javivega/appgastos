bf06877e13c530269653d00d69a1cb2f443207a6
Miguel <lowreybadboy@gmail.com>
2017-01-07 01:04:53 +0100

    anado creaccion de obetos segun tipo

--- app.js
+++ app.js
@@ -1,6 +1,50 @@
 var budgetController = (function(){
     
+    var incomes = function(id, desc, val){
+        this.id = id;
+        this.desc = desc;
+        this.val = val;
+    }
     
+    var expenses = function(id, desc, val){
+        this.id = id;
+        this.desc = desc;
+        this.val = val;
+    }
+    
+    var data = {
+        allItems: {
+            exp: [],
+            inc: []
+        },
+        
+        totals: {
+            exp: 0,
+            inc: 0
+        }
+    }
+    
+    return {
+        addItem: function(type, desc, val){
+            var element, ID;
+            
+            if(data.allItems[type].length > 0){
+                ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+            } else {
+                ID = 0;
+            }
+            
+            
+            if(type === 'exp'){
+                element = new expenses(ID, desc, val);
+            } else if(type === 'inc'){
+                element = new incomes(ID, desc, val);
+            }
+            
+            data.allItems[type].push(element);
+            return element;
+        }
+    }
     
     
     
@@ -45,20 +89,34 @@ var Controller = (function(bdgCtrl, uiCtrl){
     var ctrlAddItem = function(){
         var getInputs = uiCtrl.getInputData();
         console.log(getInputs);
+        var getItem = bdgCtrl.addItem(getInputs.type, getInputs.description, getInputs.value);
+        console.log(getItem);
     }
   
-
+    var setupEventListeners = function(){
+        document.querySelector(DOM.addButton).addEventListener('click', ctrlAddItem);
     
-    
-    document.querySelector(DOM.addButton).addEventListener('click', ctrlAddItem);
-    
-    document.addEventListener('keypress', function(event){
+        document.addEventListener('keypress', function(event){
         
         if(event.keyCode === 13 || event.which === 13){
             ctrlAddItem();
         }
     })
+    }
+    
+    return {
+        init: function(){
+            console.log('configurado');
+            setupEventListeners();
+        }
+    }
+    
+    
+    
     
     
     
 })(budgetController, UiController);
+
+
+Controller.init();
